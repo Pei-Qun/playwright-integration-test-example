2c00d00165cd004e1ea347f62cdf7ddbb4c86994
Zoie Tseng <zoie.tseng@appier.com>
2023-12-24 16:02:24 +0800

    add: mock env

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 async function enableMocking() {
-  // if (process.env.NODE_ENV !== "development") {
-  //   return;
-  // }
-  // const { worker } = await import("../mock/browser");
-  // return worker.start();
+  if (process.env.NODE_ENV !== "mock") {
+    return;
+  }
+  const { worker } = await import("../mock/browser");
+  return worker.start();
 }
 
 enableMocking().then(() => {
